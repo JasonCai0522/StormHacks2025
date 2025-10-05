12f4dbcf7b5e19bf93300e8a69c0d0767e5398d8
oliverancheta123 <oaa20@sfu.ca>
2025-10-04 17:19:22 -0700

    oliver2

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Button from './Button'
 
 const Header = () => {
   return (
-    <header>
-        <h1>Header</h1>
+
+    <header class="App-header">
+        <h1>GOGGIFY</h1>
+        <div className="button-group">
+          <Button className = "register-button" text="About Us" onClick={() => console.log("About Clicked")} />
+          <Button className = "login-button" text="Register" onClick={() => console.log("Register clicked")} />
+          <Button className = "login-button" text="Login" onClick={() => console.log("Login clicked")} />
+        </div>
     </header>
   )
 }
